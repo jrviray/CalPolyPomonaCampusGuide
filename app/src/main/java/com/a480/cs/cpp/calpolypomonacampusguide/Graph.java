85f4b2bf35fc4012602d0a1dfbeeea02bd387b1d
jrviray <jrviray@cpp.edu>
2017-04-20 23:24:45 -0700

    Initial commit and project upload with rough architecture layout

---
+++ app/src/main/java/com/a480/cs/cpp/calpolypomonacampusguide/Graph.java
@@ -0,0 +1,8 @@
+package com.a480.cs.cpp.calpolypomonacampusguide;
+
+/**
+ * Created by king on 4/20/17.
+ */
+
+public class Graph {
+}
---
+++ app/src/main/java/com/a480/cs/cpp/calpolypomonacampusguide/MapNodeActivity.java
@@ -0,0 +1,13 @@
+package com.a480.cs.cpp.calpolypomonacampusguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapNodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_node);
+    }
+}
